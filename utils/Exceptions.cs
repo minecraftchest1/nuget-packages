5a824ddde5e516cb315c0b7a909e13628eb8c44d
minecraftchest1 <minecraftchest1@outlook.com>
2021-05-27 11:15:31 -0500

    Add class designer.

---
+++ utils/Exceptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Minecraftchest1
+{
+    partial class Utils
+    {
+        [System.Serializable]
+        public class InputErrorException : Exception
+        {
+            public InputErrorException() { }
+            public InputErrorException(string message) : base(message) { }
+            public InputErrorException(string message, Exception inner) : base(message, inner) { }
+            protected InputErrorException(
+              System.Runtime.Serialization.SerializationInfo info,
+              System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
+            public string Input;
+        }
+    }
+}
