80eb5b5f00e2e643e96ad32248f9113a69841a8d
minecraftchest1 <minecraftchest1@outlook.com>
2021-05-28 09:05:42 -0500

    Update build.ps1.

--- build.ps1
+++ build.ps1
@@ -1,3 +1,33 @@
-dotnet.exe build .\utils\
-doxygen.exe Doxyfile
-Robocopy.exe .\html\ docs
+if ( Test-Path utils)
+{
+	dotnet.exe build .\utils\
+}
+else
+{
+	echo "The 'utils' folder does not exist."
+	exit
+}
+if ( Test-Path utils)
+{
+	doxygen.exe Doxyfile
+}
+else
+{
+	echo "The 'Doxyfile' file does not exist."
+if ( Test-Path html\)
+{
+	if ( Test-Path docs )
+	{
+		Robocopy.exe .\html\ docs		
+	}
+	else
+	{
+		New-Item -Type Directory docs
+		Robocopy.exe .\html\ docs
+	}
+}
+else
+{
+	echo "The 'html' folder does not exist."
+	exit
+}
