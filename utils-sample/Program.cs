49d30e34ac3399a16bb18ac15175ee394d81f41f
minecraftchest1 <minecraftchest1@outlook.com>
2021-06-21 12:44:26 -0500

    Add sample client.

---
+++ utils-sample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Minecraftchest1;
+
+namespace utils_sample
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Utils.Input("Input as string: ");
+            try
+            {
+                Utils.InputDouble("Input as double: ");
+            }
+            catch
+            {
+                Console.WriteLine("[ERROR] Input is not a double.");
+            }
+            string TestString="Me, You, Minecraftchest1; Minecraftchest2";
+            char[] Seperators = new char[] { ',', ';'};
+            Console.WriteLine($"String: {TestString}");
+            Console.WriteLine($"Seperators: ',' ';'");
+            foreach (var sub in StringToArray(TestString, Seperators))
+            {
+                Console.WriteLine("Substring: {0}", sub.Trim());
+            }
+
+            Utils.Pause();
+        }
+    }
+}
