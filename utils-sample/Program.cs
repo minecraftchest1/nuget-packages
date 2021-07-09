465f7a5e8ad02ee11c88b8f8fd993f8c7074935a
minecraftchest1 <minecraftchest1@outlook.com>
2021-07-09 08:58:31 -0500

    Add local changes.

--- utils-sample/Program.cs
+++ utils-sample/Program.cs
@@ -5,28 +5,38 @@ namespace utils_sample
 {
     class Program
     {
-        
-        static void Main(string[] args)
+        static string ArrayToString(string[] Input, char Seperator)
         {
-            Utils.Input("Input as string: ");
-            try
+            string Out;
+            for (int i = 0; i < Input.Length; i++)
             {
-                Utils.InputDouble("Input as double: ");
+                Out += Input[i];
             }
-            catch
-            {
-                Console.WriteLine("[ERROR] Input is not a double.");
-            }
-            string TestString="Me, You, Minecraftchest1; Minecraftchest2";
-            char[] Seperators = new char[] { ',', ';'};
-            Console.WriteLine($"String: {TestString}");
-            Console.WriteLine($"Seperators: ',' ';'");
-            foreach (var sub in StringToArray(TestString, Seperators))
-            {
-                Console.WriteLine("Substring: {0}", sub.Trim());
-            }
-
-            Utils.Pause();
+            return Out;
         }
+    static void Main(string[] args)
+    {
+        Utils.Input("Input as string: ");
+        try
+        {
+            Utils.InputDouble("Input as double: ");
+        }
+        catch
+        {
+            Console.WriteLine("[ERROR] Input is not a double.");
+        }
+        /*string TestString = "Me, You, Minecraftchest1; Minecraftchest2";
+        char[] Seperators = new char[] { ',', ';' };
+        Console.WriteLine($"String: {TestString}");
+        Console.WriteLine($"Seperators: ',' ';'");
+        foreach (var sub in Utils.StringToArray(TestString, Seperators))
+        {
+            Console.WriteLine("Substring: {0}", sub.Trim());
+        }*/
+
+        string[] TestArray = new string[]{'Hello','World'};
+
+        Utils.Pause();
     }
+}
 }
